f17e2152354d5e7f2ef8e8937249448a61e92f4b
Hu Bin <hubin.beijing@aliyun.com>
2016-10-06 16:58:35 +0800

    Exercise 3

---
+++ ch02/ch02_e03.php
@@ -0,0 +1,73 @@
+﻿<?php
+	$hamburger = 4.95;
+	$milkshake = 1.95;
+	$cola = 0.85;
+	$tip_rate = 0.16;
+	$tax_rate = 0.075;
+
+	$list = array(
+		'hamburger'=>[$hamburger, 2],
+		'milkshake'=>[$milkshake, 1],
+		'cola'=>[$cola, 1]
+		);
+
+	$price = 0;
+	$amount = 0;
+	foreach ($list as $name => $cells) {
+		$list[$name][] = $cells[0] * $cells[1];
+		$price += $list[$name][2];
+		$amount += $list[$name][1];
+	};
+	$list['tax'] = array($tax_rate, "-", $price * $tax_rate);
+	$list['tip'] = array($tip_rate, "-", $price * $tip_rate);
+	$list['total'] = array("-", $amount, $price + $list["tax"][2] + $list["tip"][2]);
+
+	function float_or_space($value) {
+		if (is_float($value) && 0 != $value){
+			printf("<td class='td-float'>%.2f</td>", $value);
+		} else if ("" === trim($value)) {
+			print "<td></td>";
+		} else {
+			print "<td>".$value."</td>";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<style>
+		table,th,td { margin-left: auto; margin-right: auto; border:0px; border-collapse:collapse;}
+		th {background: navy; padding:6px; text-align: center; color: white;}
+		td {background: LightGoldenRodYellow; padding:6px; text-align: center; color: black;}
+		.td-float {background: LightGoldenRodYellow; padding:6px; text-align: right; color: black;}
+		.tr-divide {border-top:2px dotted black;}
+		.tr-total {border-top:2px solid black;}
+	</style>
+	<body>
+		<table>
+		<tr>
+			<th>Name</th>
+			<th>Each</th>
+			<th>Quantity</th>
+			<th>Price</th>
+		</tr>
+		<?php
+			foreach ($list as $caption => $cells) {
+				$border = "";
+				if ("tax" == $caption) {
+					$border = " class='tr-divide'";
+				} else if ("total" == $caption) {
+					$border = " class='tr-total'";
+				}
+
+				print "<tr".$border.">";
+				print "<td>".ucwords($caption)."</td>";
+				print float_or_space($cells[0]);
+				print "<td>".$cells[1]."</td>";
+				print float_or_space($cells[2]);
+				print "</tr>";
+			}
+		?>
+		</table>
+	</body>
+</html>
